22d4f1b7f1cd0584752a902abd84a07f1f4b92d9
ved1985prakash3 <ved1985prakash3@gmail.com>
2023-05-07 22:08:13 +0200

    Update streamlit_app.py
    added the further

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 streamlit.title("My Parents New Healthy Dinner")
 print(" ")
 streamlit.title("BreakFast Menu")
+print("Omega 3 & Blue Berry Oat meal")
+print("Kale, Spinach & Rocket Smoothie")
+print("Hard Boiled free Range Egg")
