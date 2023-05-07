df68919365769f9a102b67f65ec485b3438036e8
ved1985prakash3 <ved1985prakash3@gmail.com>
2023-05-07 22:15:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text("🥣 Omega 3 & Blue Berry Oat meal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard Boiled free Range Egg")
 streamlit.text("🥑🍞 Avacado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
